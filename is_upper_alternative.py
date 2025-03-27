27077987a8fc35535666787d9b2f23f79bd2cd23
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 02:17:23 +0800

    added a function where it checks the characters according to their ascii values

--- is_upper_alternative.py
+++ is_upper_alternative.py
@@ -1,8 +1,8 @@
 #ask the user to input a word/words
 string = input("Please enter a word: ")
 #check all the characters in the input
-for char in string
 #convert characters to its unicode value using ord
 #using ASCII, check if the character is within the letters A - Z in their unicode values
+(65 <= ord(char) <= 90 for char in string if char.isalpha())
 #check if all the values are true
 #print if it is upper
