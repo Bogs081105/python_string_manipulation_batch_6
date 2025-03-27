6fac553a8e082271e9f0969ec5dc13beb6ee8daa
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 00:25:48 +0800

    initialized a new variable

--- swapcase_alternative.py
+++ swapcase_alternative.py
@@ -1,5 +1,6 @@
 #initialize a new variable to add the changed casings
 #ask the user to input words with improper casing
+swapcase_alt = ""
 user_input = input("Please enter word/s with improper casing: ")
 #use isupper and islower to check the casing
 #using for loop and char function, change characters inside the input.
