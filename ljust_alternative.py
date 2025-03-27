ba0a6d781aa3f56bc4ec1ad61782c0e81ffc13c6
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 03:29:01 +0800

    added a print function

--- ljust_alternative.py
+++ ljust_alternative.py
@@ -4,3 +4,4 @@ spaces = int(input("Enter how many spaces you would like to be left justified: "
 #use string concatenation to add empty spaces along with how many spaces they would desire
 string = " " * spaces + string
 #print output
+print(string)
