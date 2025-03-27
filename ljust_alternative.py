6f5273345dc6d52d036b2e2c6d249a86feab0d42
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 03:27:08 +0800

    added input functions

--- ljust_alternative.py
+++ ljust_alternative.py
@@ -1,3 +1,7 @@
 #ask the user to input a string and the desired spaces of their word
+string = input("Please enter a word/s: ")
+spaces = int(input("Enter how many spaces you would like to be left justified: "))
 #use string concatenation to add empty spaces along with how many spaces they would desire
+string = " " * spaces + string
+
 #print output
