36a13155a6abf0a48621d80134eff717199c319c
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 03:08:33 +0800

    added a function where it checks if the end of the string matches the suffix

--- endswith_alternative.py
+++ endswith_alternative.py
@@ -2,7 +2,11 @@
 string = input("Enter a string: ")
 suffix = input("Enter a suffix: ")
 #using if function, check if the suffix is longer than the string
+#print the output
 if len(suffix) > len(string):
     print(f"{suffix} is longer than {string}, {string} does not end with {suffix}")
 #check and slice the end of the string to check if it matches the suffix
-#print the output
+elif string[-len(suffix):] == suffix:
+    print(f"{string}ends with {suffix}")
+else:
+    print(f"{string}does not end with{suffix}")
