c01d1ec79a6a3bf05656d28900de0a5a1df1176a
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 02:57:55 +0800

    added variables to modify

--- endswith_alternative.py
+++ endswith_alternative.py
@@ -1,4 +1,6 @@
 #input variables: String and Suffix
+string = input("Enter a string: ")
+suffix = input("Enter a suffix: ")
 #using if function, check if the suffix is longer than the string
 #check and slice the end of the string to check if it matches the suffix
 #print the output
