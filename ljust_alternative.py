ca024ba5e6902b523d5fd993e6ff5da33e5cb678
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 03:28:42 +0800

    added a function where it adds the number of spaces entered into the string to give a justified effect

--- ljust_alternative.py
+++ ljust_alternative.py
@@ -3,5 +3,4 @@ string = input("Please enter a word/s: ")
 spaces = int(input("Enter how many spaces you would like to be left justified: "))
 #use string concatenation to add empty spaces along with how many spaces they would desire
 string = " " * spaces + string
-
 #print output
