6de961cb44da5164c519dac9a77fc3adf00316a4
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 00:25:03 +0800

    updated the pseudo code's order

--- swapcase_alternative.py
+++ swapcase_alternative.py
@@ -1,5 +1,5 @@
-#ask the user to input words with improper casing
 #initialize a new variable to add the changed casings
+#ask the user to input words with improper casing
 user_input = input("Please enter word/s with improper casing: ")
 #use isupper and islower to check the casing
 #using for loop and char function, change characters inside the input.
