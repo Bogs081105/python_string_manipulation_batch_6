7f4ad04142a7fca3003bb03ff3f2e3b412599690
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 00:07:49 +0800

    added parentheses beside casefold to call the function and to fix the error

--- lower_function_alternative.py
+++ lower_function_alternative.py
@@ -1,5 +1,6 @@
 #ask the user to input string with upper cases
 with_upper = input("Please input a string with upper cases: ")
 #using casefold, convert the strings into lower cases
-lower_alt = with_upper.casefold
+lower_alt = with_upper.casefold()
 #print the output
+print(lower_alt)
