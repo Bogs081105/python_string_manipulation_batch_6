755bf4488ef9818ce97d3eb4d35c53713cdc2520
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 02:08:59 +0800

    added a pseudo code

---
+++ is_upper_alternative.py
@@ -0,0 +1,6 @@
+#ask the user to input a word/words
+#convert characters to its unicode value using ord
+#using ASCII, check if the character is within the letters A - Z in their unicode values
+#check all the characters in the input
+#check if all the values are true
+#print if it is upper
