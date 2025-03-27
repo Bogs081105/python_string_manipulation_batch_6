a95a68ecb7ba6dbea8d38d5558fcf2b6385e7d13
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 02:13:18 +0800

    fixed pseudo code

--- is_upper_alternative.py
+++ is_upper_alternative.py
@@ -1,7 +1,7 @@
 #ask the user to input a word/words
 string = input("Please enter a word: ")
+#check all the characters in the input
 #convert characters to its unicode value using ord
 #using ASCII, check if the character is within the letters A - Z in their unicode values
-#check all the characters in the input
 #check if all the values are true
 #print if it is upper
