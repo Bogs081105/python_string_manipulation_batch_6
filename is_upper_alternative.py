aecc98c85969b3e8c04c4985b743bbb5724d268a
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 02:09:55 +0800

    added an input function

--- is_upper_alternative.py
+++ is_upper_alternative.py
@@ -1,4 +1,5 @@
 #ask the user to input a word/words
+string = input("Please enter a word: ")
 #convert characters to its unicode value using ord
 #using ASCII, check if the character is within the letters A - Z in their unicode values
 #check all the characters in the input
