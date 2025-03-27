162cf160824e6722047d06503c1388ebde6819f2
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 03:46:03 +0800

    added a function to capitalize every split words

--- title_alternative.py
+++ title_alternative.py
@@ -2,4 +2,5 @@
 user_input = input("Enter a word/s or a sentence: ")
 #split the strings
 #capitalize every starting letter from the split words
+title_case = " ".join(word.capitalize() for word in user_input.split())
 #print the output
