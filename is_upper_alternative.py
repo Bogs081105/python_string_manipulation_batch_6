5e69dbc0da40ad527db2c3feacbf3e8fa8dbfb61
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 02:14:20 +0800

    added for loop to check all characters continuosly

--- is_upper_alternative.py
+++ is_upper_alternative.py
@@ -1,6 +1,7 @@
 #ask the user to input a word/words
 string = input("Please enter a word: ")
 #check all the characters in the input
+for char in string
 #convert characters to its unicode value using ord
 #using ASCII, check if the character is within the letters A - Z in their unicode values
 #check if all the values are true
