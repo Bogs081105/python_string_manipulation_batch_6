069a0ddcb450183f894e6ce03fd7cc26e620888e
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-27 22:53:13 +0800

    removed space and print as it will print spaces inside the word, will try to remove only the left side

--- remove_space_without_lstrip.py
+++ remove_space_without_lstrip.py
@@ -1,6 +1,5 @@
 #ask the user to input a word or a sentence that has space characters at the beginning
 words = input("Please enter word/s with spaces in the beginning: ")
 #create a function that removes the space characters
-no_spaces = words.replace(" ", "")
+no_spaces 
 #print the word without spaces
-print(no_spaces) 
