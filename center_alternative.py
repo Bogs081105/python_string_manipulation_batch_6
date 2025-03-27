518cbd0e839f93c751567a86624eca09fc672743
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 03:54:37 +0800

    added f-string formatting

--- center_alternative.py
+++ center_alternative.py
@@ -1,3 +1,5 @@
 #ask the user to input variables: string and width of the entire word
+user_input = input("Enter a word/s: ")
+width = int(input("Enter the desired width of the entire word: "))
 #use f-string formatting to center align (^)
 #print the output
