de6d41859a3b46d2c981dc415b21e289399bbd29
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 00:03:35 +0800

    added input function'

--- lower_function_alternative.py
+++ lower_function_alternative.py
@@ -1,3 +1,4 @@
 #ask the user to input string with upper cases
+with_upper = input("Please input a string with upper cases: ")
 #using casefold, convert the strings into lower cases
 #print the output
