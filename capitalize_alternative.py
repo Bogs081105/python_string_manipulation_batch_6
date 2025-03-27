bd397ab14278fde8a2e60dea29aba0973237cdb7
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 00:51:05 +0800

    added a function where input is modified into capitalize format without actually calling it

--- capitalize_alternative.py
+++ capitalize_alternative.py
@@ -1,4 +1,5 @@
 #ask the user to input word/s with improper casing
 user_input = input("Please enter word/s with improper casing: ")
 #using indexing, upper, and lower, modify the code in capitalize format
+user_input = (user_input[0].upper() + user_input[1:].lower())
 #print output
