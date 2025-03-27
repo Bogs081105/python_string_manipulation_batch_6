8082e3c9982b9f21552ac616fad02d2840aa76d8
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 02:20:09 +0800

    added a function where it checks if the statement is true

--- is_upper_alternative.py
+++ is_upper_alternative.py
@@ -1,8 +1,9 @@
 #ask the user to input a word/words
 string = input("Please enter a word: ")
+
 #check all the characters in the input
 #convert characters to its unicode value using ord
 #using ASCII, check if the character is within the letters A - Z in their unicode values
-(65 <= ord(char) <= 90 for char in string if char.isalpha())
 #check if all the values are true
+upper_alt = all(65 <= ord(char) <= 90 for char in string if char.isalpha())
 #print if it is upper
