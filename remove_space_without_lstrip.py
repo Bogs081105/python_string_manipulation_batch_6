ebaa560888a953ad49b61177ff2e13b76dcd7de3
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-27 22:49:16 +0800

    added a function that removes spaces

--- remove_space_without_lstrip.py
+++ remove_space_without_lstrip.py
@@ -1,5 +1,5 @@
 #ask the user to input a word or a sentence that has space characters at the beginning
 words = input("Please enter word/s with spaces in the beginning: ")
-
 #create a function that removes the space characters
+no_spaces = words.replace(" ", "")
 #print the word without spaces
