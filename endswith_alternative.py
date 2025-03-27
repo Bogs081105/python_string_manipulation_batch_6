4ba65ea3f88f04f828e96641aeeb141bad94c59f
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 03:04:09 +0800

    added a function where it checks if the suffix is longer than the string

--- endswith_alternative.py
+++ endswith_alternative.py
@@ -2,5 +2,7 @@
 string = input("Enter a string: ")
 suffix = input("Enter a suffix: ")
 #using if function, check if the suffix is longer than the string
+if len(suffix) > len(string):
+    print(f"{suffix} is longer than {string}, {string} does not end with {suffix}")
 #check and slice the end of the string to check if it matches the suffix
 #print the output
