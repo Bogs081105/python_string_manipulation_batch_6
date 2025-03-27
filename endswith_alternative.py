086ecf763798cf688c1e163a5d64042c49b925ac
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 02:56:28 +0800

    pseudo code created

---
+++ endswith_alternative.py
@@ -0,0 +1,4 @@
+#input variables: String and Suffix
+#using if function, check if the suffix is longer than the string
+#check and slice the end of the string to check if it matches the suffix
+#print the output
