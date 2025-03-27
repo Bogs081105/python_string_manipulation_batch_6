f9fc3a9404b8f908358a6f460b98d75fe050d177
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 01:56:48 +0800

    fixed an error where the string wasnt updated

--- remove_prefix_alternative.py
+++ remove_prefix_alternative.py
@@ -5,6 +5,6 @@ prefix = ("Pogi")
 #check if the word has a prefix that has been set
 if string.startswith(prefix):
 #remove the prefix
-    string.replace(prefix, "", 1)
+    string = string.replace(prefix, "", 1)
 #print the result
 print(string)
