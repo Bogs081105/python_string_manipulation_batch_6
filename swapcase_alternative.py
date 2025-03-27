28f709e6a26e00bdf06a83df6563aa8f2bcb488b
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 00:27:41 +0800

    updated pseudo code's order

--- swapcase_alternative.py
+++ swapcase_alternative.py
@@ -2,9 +2,10 @@
 #ask the user to input words with improper casing
 swapcase_alt = ""
 user_input = input("Please enter word/s with improper casing: ")
-#use isupper and islower to check the casing
 #using for loop and char function, change characters inside the input.
 for char in user_input:
 #use if statements to swap the cases
+#use isupper and islower to check the casing
+    if char.isupper
 #print the result
 print(user_input)
