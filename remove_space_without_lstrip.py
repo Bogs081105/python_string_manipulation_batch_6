8ff05d28d3aaf1c7224a832a0520dfee0e170cee
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-27 22:56:29 +0800

    updated pseudo code to try other function

--- remove_space_without_lstrip.py
+++ remove_space_without_lstrip.py
@@ -1,5 +1,6 @@
 #ask the user to input a word or a sentence that has space characters at the beginning
 words = input("Please enter word/s with spaces in the beginning: ")
 #create a function that removes the space characters
-no_spaces 
+#use while loop and startswith function
+#using index and slice, remove the white spaces
 #print the word without spaces
