df203f51e71998d691f3d04c22655538b2b1b9b6
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 00:26:48 +0800

    added for loop and char function'

--- swapcase_alternative.py
+++ swapcase_alternative.py
@@ -4,6 +4,7 @@ swapcase_alt = ""
 user_input = input("Please enter word/s with improper casing: ")
 #use isupper and islower to check the casing
 #using for loop and char function, change characters inside the input.
+for char in user_input:
 #use if statements to swap the cases
 #print the result
 print(user_input)
