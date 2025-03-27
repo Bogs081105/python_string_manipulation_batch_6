ab61b15a5556ce6da2819c22aa8afecd7a7766d2
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-28 00:51:42 +0800

    added print function

--- capitalize_alternative.py
+++ capitalize_alternative.py
@@ -3,3 +3,4 @@ user_input = input("Please enter word/s with improper casing: ")
 #using indexing, upper, and lower, modify the code in capitalize format
 user_input = (user_input[0].upper() + user_input[1:].lower())
 #print output
+print(user_input)
