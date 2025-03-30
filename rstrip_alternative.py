65e455be14fbebf8a53da36da02a142193609a43
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-30 14:26:44 +0800

    updated the code to slice the negative index with spaces

--- rstrip_alternative.py
+++ rstrip_alternative.py
@@ -4,4 +4,5 @@ user_input = input("Please enter a string with spaces after: ")
 #use negative indexing to check the last character
 while user_input and user_input[-1] == " ":
 #slice the negative index to remove the spaces
+    user_input = user_input[:-1]
 #print using repr() to show quotes
