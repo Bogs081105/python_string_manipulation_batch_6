5c8e41b7966912979dd6077a0aef21d5e4ef1190
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-30 14:22:55 +0800

    pseudo code created

---
+++ rstrip_alternative.py
@@ -0,0 +1,5 @@
+#ask the user to input a string with spaces after the string
+#use a loop to continuously check until there are no more spaces at the end
+#use negative indexing to check the last character
+#slice the negative index to remove the spaces
+#print using repr() to show quotes
