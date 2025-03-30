482f4a7ff459a62f3c30869b58d0b1db18639da9
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-30 14:24:55 +0800

    added a while function to continuously check for empty spaces

--- rstrip_alternative.py
+++ rstrip_alternative.py
@@ -2,5 +2,6 @@
 user_input = input("Please enter a string with spaces after: ")
 #use a loop to continuously check until there are no more spaces at the end
 #use negative indexing to check the last character
+while user_input and user_input[-1] == " ":
 #slice the negative index to remove the spaces
 #print using repr() to show quotes
