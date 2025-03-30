2ea7da3d2bd72eae778bf547b08949782cb6235e
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-30 15:17:48 +0800

    pseudo code created

---
+++ upper_alternative.py
@@ -0,0 +1,7 @@
+#ask the user to input lowercase string
+#initialize an empty string to input the capitalized letters
+#use for loop to check characters inside the string
+#use if else to check if it is a lowercase letter
+#convert using ASCII values (-32) as there are 32 items between lower caps \n 
+# and upper caps letters
+#print the result
