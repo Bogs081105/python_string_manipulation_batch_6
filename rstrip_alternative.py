b486c4aa188536298cf8ce62eb863346aea76cbb
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-30 14:23:48 +0800

    added input function

--- rstrip_alternative.py
+++ rstrip_alternative.py
@@ -1,4 +1,5 @@
 #ask the user to input a string with spaces after the string
+user_input = input("Please enter a string with spaces after: ")
 #use a loop to continuously check until there are no more spaces at the end
 #use negative indexing to check the last character
 #slice the negative index to remove the spaces
