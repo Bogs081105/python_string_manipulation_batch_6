bb1355eb1231bcb290ab2cf097c94921cad5cace
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 16:57:44 +0800

    fixed code to look more like the rjust function

--- rjust_alternative.py
+++ rjust_alternative.py
@@ -1,7 +1,9 @@
 #ask the user to input a string and desired spaces of their word
 user_input = input("Please input a string: ")
-spaces = int(input("Please input desired spaces you would like to put on the right: "))
+width = int(input("Please input the desired width: "))
 #use string concatenation to add empty spaces along with how many spaces they would desire
+#calculated the spaces needed to reach desired width
+spaces = max(0, width - len(user_input))
 result = " " * spaces + user_input
 #print result
 print(f"The string right justified: {result}")
