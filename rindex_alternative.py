d59de24fa598cf6058f9bd4ba0d28cf0be8fb1ca
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 17:57:23 +0800

    input functions added

--- rindex_alternative.py
+++ rindex_alternative.py
@@ -1,4 +1,6 @@
 #ask the user to input 2 variables: main string and the substring
+main_string = input("Please enter the main word/sentence: ")
+substring = input("Please enter the word to look for: ")
 #using rfind, find the substring inside the string(set it as the variable position)
 #check if the substring was found using if else statement
 #print if found or not found
