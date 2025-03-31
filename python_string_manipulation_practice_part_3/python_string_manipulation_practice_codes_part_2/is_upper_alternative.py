824408effbcb790d81e3b79bba259d228c85d1ab
Bogs081105 <tubogkyrie0811@gmail.com>
2025-04-01 02:11:33 +0800

    Moved repositories to respective folders

---
+++ python_string_manipulation_practice_part_3/python_string_manipulation_practice_codes_part_2/is_upper_alternative.py
@@ -0,0 +1,13 @@
+#ask the user to input a word/words
+string = input("Please enter a word: ")
+
+#check all the characters in the input
+#convert characters to its unicode value using ord
+#using ASCII, check if the character is within the letters A - Z in their unicode values
+#check if all the values are true
+isupper_alt = all(65 <= ord(char) <= 90 for char in string if char.isalpha())
+#print if it is upper
+if isupper_alt:
+    print("The string is upper case")
+else:
+    print("The string is not upper case")
---
+++ python_string_manipulation_practice_part_3/python_string_manipulation_practice_codes_part_2/swapcase_alternative.py
@@ -0,0 +1,17 @@
+#initialize a new variable to add the changed casings
+#ask the user to input words with improper casing
+swapcase_alt = ""
+user_input = input("Please enter word/s with improper casing: ")
+#using for loop and char function, change characters inside the input.
+for char in user_input:
+#use if statements to swap the cases
+#use isupper and islower to check the casing
+#add the cases inside the empty variable
+    if char.isupper():
+        swapcase_alt += char.lower()
+    elif char.islower():
+        swapcase_alt += char.upper()
+    else:
+        swapcase_alt += char
+#print the result
+print(swapcase_alt)
