e2ee9b19c440697d340ee5841db9600ccf8c1c23
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 17:48:02 +0800

    set a variable to position to find the substring within the mainstring

--- index_alternative.py
+++ index_alternative.py
@@ -2,5 +2,6 @@
 main_string = input("Please enter the main word/sentence: ")
 substring = input("Please enter the word to look for: ")
 #using find, find the substring inside the string(set it as the variable position)
+position = main_string.find(substring)
 #check if the substring was found using if else statement
 #print if found or not found
