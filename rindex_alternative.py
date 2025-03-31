e94dc497314207dbc500a7dea536821d840096e9
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 17:54:27 +0800

    pseudo code created

---
+++ rindex_alternative.py
@@ -0,0 +1,4 @@
+#ask the user to input 2 variables: main string and the substring
+#using rfind, find the substring inside the string(set it as the variable position)
+#check if the substring was found using if else statement
+#print if found or not found
