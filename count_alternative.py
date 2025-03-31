bbd3b702d8c52da9a4f8ef245b5a6068377d9c30
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 17:29:24 +0800

    pseudo code created

---
+++ count_alternative.py
@@ -0,0 +1,5 @@
+#ask the user to input variables: Main string and substring
+#initialize a counter to store how many times the substring occured
+#using for loop and if statements, iterate through the string \n
+#  and check for similar words using slice method
+#print the result
