6f287abe5d8df8a81b4b41ecd6b482f450c3765b
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 15:55:44 +0800

    set a suffix

--- removesuffix_alternative.py
+++ removesuffix_alternative.py
@@ -1,6 +1,7 @@
 #set a sample string
 string = ("Sir Danilo Pogi")
 #set a suffix
+suffix = ("Pogi")
 #check if the word has a suffix that has been set using endswith
 #remove the suffix
 #print the result
