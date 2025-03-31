47fdffdfb297e553bb3a3c2962adb77ad6347d97
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 17:31:26 +0800

    initialized a counter

--- count_alternative.py
+++ count_alternative.py
@@ -2,6 +2,7 @@
 user_input = input("Please input a complete word/sentence: ")
 substring = input("Please input a word to count: ")
 #initialize a counter to store how many times the substring occured
+count = 0
 #using for loop and if statements, iterate through the string \n
 #  and check for similar words using slice method
 #print the result
