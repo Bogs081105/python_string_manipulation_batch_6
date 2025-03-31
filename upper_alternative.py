6e8d9fb987c7cfb45b3956b5d97bae81b93df3ab
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 14:50:36 +0800

    added for loop

--- upper_alternative.py
+++ upper_alternative.py
@@ -3,6 +3,7 @@ user_input = input("Please input a lowercase string: ")
 #initialize an empty string to input the capitalized letters
 upper_alt = ""
 #use for loop to check characters inside the string
+for char in user_input:
 #use if else to check if it is a lowercase letter
 #convert using ASCII values (-32) as there are 32 items between lower caps \n 
 # and upper caps letters
