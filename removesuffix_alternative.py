89cc9e601ec2955dbaa7132671a097d76092970b
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 15:58:38 +0800

    added function to remove suffix

--- removesuffix_alternative.py
+++ removesuffix_alternative.py
@@ -5,4 +5,5 @@ suffix = ("Pogi")
 #check if the word has a suffix that has been set using 
 if string.endswith(suffix):
 #remove the suffix
+    string = string.replace("", suffix)
 #print the result
