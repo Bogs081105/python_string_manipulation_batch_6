dbce92c3e7ed433bd41ca47cd78fe4aadf59db3f
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 15:21:33 +0800

    added print in pseudo code

--- rjust_alternative.py
+++ rjust_alternative.py
@@ -1,3 +1,4 @@
 #ask the user to input a string and desired spaces of their word
 #calculate how many spaces to add to achieve the desired width
 #use string concatenation to add empty spaces along with how many spaces they would desire
+#print result
