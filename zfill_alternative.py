dd6fca7e93f065eb8a5d1b3660ccd2e7db82e212
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 17:05:45 +0800

    pseudo code added

---
+++ zfill_alternative.py
@@ -0,0 +1,4 @@
+#ask the user input variables: String and Width of the word
+#calculate how many spaces the code should add
+#using string concatenation, add spaces according to the desired width
+#print the result
