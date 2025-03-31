665a117e799263e63d4be2eb8f118be4b3f5b475
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 17:02:39 +0800

    adjusted code to look more like ljust by getting the desired width instead of how many spaces the user should add

--- ljust_alternative.py
+++ ljust_alternative.py
@@ -1,7 +1,8 @@
 #ask the user to input a string and the desired spaces of their word
 string = input("Please enter a word/s: ")
-spaces = int(input("Enter how many spaces you would like to be left justified: "))
+width = int(input("Enter the desired width: "))
 #use string concatenation to add empty spaces along with how many spaces they would desire
+spaces = max(0, width - len(string))
 string = string + " " * spaces
 #print output
 print(string)
