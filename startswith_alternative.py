9f6592512f43dfe43f0910b1136535f8d9fab6a9
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 16:41:35 +0800

    added input functions

--- startswith_alternative.py
+++ startswith_alternative.py
@@ -1,3 +1,5 @@
 #input variables: String and Prefix
+string = input("Please input a string: ")
+prefix = input("Please enter a prefix: ")
 #check and slice the end of the string to check if it matches the prefix
 #print the output
