4753574fb6c79df9b6452d35c623818644dd7086
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 15:30:30 +0800

    fixed pseudo code

--- rjust_alternative.py
+++ rjust_alternative.py
@@ -1,6 +1,5 @@
 #ask the user to input a string and desired spaces of their word
 user_input = input("Please input a string: ")
 spaces = int(input("Please input desired spaces you would like to put on the right: "))
-#calculate how many spaces to add to achieve the desired width
 #use string concatenation to add empty spaces along with how many spaces they would desire
 #print result
