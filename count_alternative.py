b4d7f021947751dc9bacbd560dbc0f2371265d2e
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 17:31:01 +0800

    added input variables

--- count_alternative.py
+++ count_alternative.py
@@ -1,4 +1,6 @@
 #ask the user to input variables: Main string and substring
+user_input = input("Please input a complete word/sentence: ")
+substring = input("Please input a word to count: ")
 #initialize a counter to store how many times the substring occured
 #using for loop and if statements, iterate through the string \n
 #  and check for similar words using slice method
