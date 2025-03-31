f2fe25fe9674cf7fa5ed3aadf3eba1c60c383e86
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 17:09:06 +0800

    used string concatenation to add zeros in the beginning

--- zfill_alternative.py
+++ zfill_alternative.py
@@ -4,4 +4,5 @@ width = int(input("Enter your desired width: "))
 #calculate how many spaces the code should add
 spaces = max(0, width - len(string))
 #using string concatenation, add spaces according to the desired width
+result = "0" * spaces + string
 #print the result
