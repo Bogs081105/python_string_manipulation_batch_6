a0324d9216582c9dfd5dff19df1239885308f74f
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 16:18:58 +0800

    added a print functions where it returns the value if the statements are true or false to identify if it is lower case

--- is_lower_alt.py
+++ is_lower_alt.py
@@ -7,3 +7,7 @@ for char in string:
 #check if all the values are true
     islower_alt = (97 <= ord(char) <= 122 for char in string if char.isalpha())
 #print if it is lower
+if islower_alt:
+    print("The string is in lower case")
+else:
+    print("The string is not in lower case")
