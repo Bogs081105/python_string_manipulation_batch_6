6dfcfebbd855ca7c30770ddf7e6b03789e4e3e42
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 17:06:59 +0800

    added input function

--- zfill_alternative.py
+++ zfill_alternative.py
@@ -1,4 +1,6 @@
 #ask the user input variables: String and Width of the word
+string = input("Please enter a word/s: ")
+width = int(input("Enter your desired width: "))
 #calculate how many spaces the code should add
 #using string concatenation, add spaces according to the desired width
 #print the result
