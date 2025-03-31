2b64f7d55551096b8cb533e180472373b3b403f7
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 16:06:42 +0800

    pseudo code created

---
+++ is_lower_alt.py
@@ -0,0 +1,6 @@
+#ask the user to input a word/words
+#check all the characters in the input
+#convert characters to its unicode value using ord
+#using ASCII, check if the character is within the letters a - z in their unicode values
+#check if all the values are true
+#print if it is lower
