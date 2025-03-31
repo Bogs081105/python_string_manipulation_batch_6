f1de3a89baef8727327d6a327a273378aeba78f1
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 17:59:02 +0800

    set a variable to position to rfind the substring inside the mainstring

--- rindex_alternative.py
+++ rindex_alternative.py
@@ -2,5 +2,6 @@
 main_string = input("Please enter the main word/sentence: ")
 substring = input("Please enter the word to look for: ")
 #using rfind, find the substring inside the string(set it as the variable position)
+position = main_string.rfind(substring)
 #check if the substring was found using if else statement
 #print if found or not found
