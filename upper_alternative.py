637653b07d095f296821bc2f7ee6ae8915695c09
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 14:57:25 +0800

    added if function to check if it is a lowercase letter

--- upper_alternative.py
+++ upper_alternative.py
@@ -5,6 +5,7 @@ upper_alt = ""
 #use for loop to check characters inside the string
 for char in user_input:
 #use if else to check if it is a lowercase letter
+if "a" <= char <= "z":
 #convert using ASCII values (-32) as there are 32 items between lower caps \n 
 # and upper caps letters
 #print the result
