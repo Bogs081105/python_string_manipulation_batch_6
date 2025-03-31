75161dfc999d9dee7ee44f2c91f017587123c779
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 16:15:10 +0800

    added a function where it checks the characters according to their ASCII values

--- is_lower_alt.py
+++ is_lower_alt.py
@@ -4,5 +4,6 @@ string = input("Please enter a word: ")
 for char in string:
 #convert characters to its unicode value using ord
 #using ASCII, check if the character is within the letters a - z in their unicode values
+    (97 <= ord(char) <= 122 for char in string if char.isalpha())
 #check if all the values are true
 #print if it is lower
