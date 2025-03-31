267187e534dca8092dd367facbf95ad4a3ba91bd
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 14:50:03 +0800

    initialized an empty string to store capitalized characters

--- upper_alternative.py
+++ upper_alternative.py
@@ -1,6 +1,7 @@
 #ask the user to input lowercase string
 user_input = input("Please input a lowercase string: ")
 #initialize an empty string to input the capitalized letters
+upper_alt = ""
 #use for loop to check characters inside the string
 #use if else to check if it is a lowercase letter
 #convert using ASCII values (-32) as there are 32 items between lower caps \n 
