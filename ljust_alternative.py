ff8c7a6a595b5d94d6c7865ad2048331843d6652
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-31 15:36:13 +0800

    fixed code where it justified to the right instead of left

--- ljust_alternative.py
+++ ljust_alternative.py
@@ -2,6 +2,6 @@
 string = input("Please enter a word/s: ")
 spaces = int(input("Enter how many spaces you would like to be left justified: "))
 #use string concatenation to add empty spaces along with how many spaces they would desire
-string = " " * spaces + string
+string = string + " " * spaces
 #print output
 print(string)
